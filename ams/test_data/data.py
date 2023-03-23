0a69622bb3c5f14f69de9119bfbd21e44f295c3e
Rozmin12 <117960682+Rozmin12@users.noreply.github.com>
2023-03-23 15:59:58 +0530

    Update data.py

--- ams/test_data/data.py
+++ ams/test_data/data.py
@@ -3,6 +3,7 @@ from werkzeug.security import check_password_hash, generate_password_hash
 from .. import db
 from ..models.User import User
 from ..models.Staff import Staff
+from ..models.Student import Student
 
 
 def seed_data():
@@ -10,7 +11,19 @@ def seed_data():
     password = 'password'
     user_password = generate_password_hash(password, 'sha256')  
     user1 = User(username='francis', email='francis@bolton.ac.uk', telephone='78124523', password=user_password, is_staff=True)
+    user2 = User(username='ibtisam', email='ibtisam@bolton.ac.uk', telephone='78124524', password='password', is_staff=True)
     staff1 = Staff(first_name='Francis', last_name='Morris', staff_code='7813', users=user1)
+    
+    
+    staff2=Staff(first_name='Ibtisam',last_name='Mogul', staff_code="7814",users=user2)
+    user3=User(username='rozmin',email='rozmin@bolton.ac.uk',telephone='7654789878',password='password',is_student=True)
+    student1=Student(first_name='Rozmin', last_name='Shaikh', student_no='2228266', users=user3)
+    
+    
+    
+    
+    
+    
     db.session.add_all([user1])
     db.session.add_all([staff1])
     db.session.commit()
