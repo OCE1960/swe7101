a141bb8bbe8433f39dd495b550f7ccc8663f04e0
Rozmin12 <rzmnshaikh@gmail.com>
2023-03-24 12:18:15 +0530

    Trial 4 of data.py

--- ams/test_data/data.py
+++ ams/test_data/data.py
@@ -3,10 +3,13 @@ from ..models.User import User
 from ..models.Staff import Staff
 from ..models.Student import Student
 from ..models.Course import Course
-
+from ..models.Semester import Semester
+from ..models.Module import Module
+from werkzeug.security import generate_password_hash, check_password_hash
 
 def seed_data():
     password = 'password'
+    
     user_password = generate_password_hash(password, 'sha256')   
     user1 = User(username='francis', email='francis@bolton.ac.uk', telephone='78124523', password='password', is_staff=True)
     user2 = User(username='ibtisam', email='ibtisam@bolton.ac.uk', telephone='78124524', password='password', is_staff=True)
@@ -18,7 +21,13 @@ def seed_data():
     student1=Student(first_name='Rozmin', last_name='Shaikh', student_no='2228266', users=user3)
 
     course1=Course(code=123,course_name="Masters in Software Engineering",course_description="This is the Masters course for Software engineering", course_level=7, course_credit=140)
+    
+    semester1=Semester(session=1, year=1, is_Active=True)
+    
+    module1=Module(name="Contemporary Software Practices", module_code="SWE7101", description="First Module of Msc in Software Engineering")
+    
+    moduleLesson1=moduleLesson1(venue="Richie Computing Lab", time="9:00", date="12-02-2023", checking_code="AHB123")
 
-    db.session.add_all()
+    moduleLessonAttendance1=moduleLessonAttendance1(attendance_status="Present")
     db.session.add_all([staff1])
     db.session.commit()
